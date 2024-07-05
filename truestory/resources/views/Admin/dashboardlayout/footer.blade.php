fadb2fbdd467b32242dd47db2c4c94595a765fc1
Sanjay <sanjaypanihar@gmail.com>
2024-07-05 18:28:47 +0530

    true story project delete

--- truestory/resources/views/Admin/dashboardlayout/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-        <div class="pull-right hidden-xs">
-          <b>Version</b> 1.0
-        </div>
-        <strong>Copyright &copy; 2023-2024 <a href="">ODD</a>.</strong> All rights reserved. Developed by Yourwebstore.
-      </footer>
