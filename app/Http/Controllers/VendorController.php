671824e83dedbc1b57f2bb0eef118f6ea5ff76ba
Sanjay <sanjaypanihar@gmail.com>
2024-07-06 14:07:49 +0530

    update password url update

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -194,7 +194,7 @@ class VendorController extends Controller
         );
 
         return $status == Password::PASSWORD_RESET
-            ? redirect()->route('login')->with('status', __($status))
+            ? redirect('AdminLogin')->with('status', __($status))
             : back()->withErrors(['email' => [__($status)]]);
     }
 
